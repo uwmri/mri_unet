33d5c195123345389369b221379f73e21ea78f51
Kevin M. Johnson <kmjohnson3@wisc.edu>
2021-06-24 17:28:47 -0500

    Update testing

--- testing/test_mri_unet.py
+++ testing/test_mri_unet.py
@@ -1,33 +1,40 @@
+import unittest
+from mri_unet import unet
 import torch
-import mri_unet
-
-from mri_unet import *
 from torchsummary import summary
 
-in_channels = 8
-out_channels = 2
+if __name__ == '__main__':
+    unittest.main()
 
-for complex_kernel in [True, False]:
-    for complex_input in [True, False]:
-        for ndims in [2,3]:
-            model = MRI_UNet(in_channels,
-                             out_channels,
-                             f_maps=64,
-                             layer_order='cr',
-                             num_groups=0,
-                             depth=2,
-                             layer_growth=2.0,
-                             residual=True,
-                             complex_input=complex_input,
-                             complex_kernel=complex_kernel,
-                             ndims=ndims)
-            model.cuda()
 
-            input_size = (in_channels,)
-            for d in range(ndims):
-                input_size += (128,)
+class TestMRIUNet(unittest.TestCase):
 
-            if complex_input:
-                summary(model, input_size, dtypes=[torch.complex64, ])
-            else:
-                summary(model, input_size, dtypes=[torch.float32, ])
+    def test_unet(self):
+
+        in_channels = 8
+        out_channels = 2
+
+        for complex_kernel in [True, False]:
+            for complex_input in [True, False]:
+                for ndims in [2, 3]:
+                    model = unet.MRI_UNet(in_channels,
+                                          out_channels,
+                                          f_maps=64,
+                                          layer_order='cr',
+                                          num_groups=0,
+                                          depth=2,
+                                          layer_growth=2.0,
+                                          residual=True,
+                                          complex_input=complex_input,
+                                          complex_kernel=complex_kernel,
+                                          ndims=ndims)
+                    model.cuda()
+
+                    input_size = (in_channels,)
+                    for d in range(ndims):
+                        input_size += (128,)
+
+                    if complex_input:
+                        summary(model, input_size, dtypes=[torch.complex64, ])
+                    else:
+                        summary(model, input_size, dtypes=[torch.float32, ])
